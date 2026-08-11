ae0fcd0d6991b54fa13b45af0da5b38b2ae366b5
Harsha Sain <harshahere02@gmail.com>
2026-08-11 23:56:26 +0530

    Setting up database of the backend

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/$
+            {DB_NAME}`)
+            app.on("error", (error) => {
+                console.log("Error", error);
+                throw error
+            })
+
+            app.listen(process.env.PORT, () => {
+                console.log(`App is listenning on port $
+                    {process.env.PORT}`);
+            })
+    } catch (error) {
+        console.error("Erro:" , error)
+        throw err
+    }
+})()
+    */

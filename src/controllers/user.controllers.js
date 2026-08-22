0e050d8e89f53afa5fe031e82b7f9545035303b1
Harsha Sain <harshahere02@gmail.com>
2026-08-22 22:53:24 +0530

    Add register controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,4 +1,8 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";  
 
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend
@@ -11,6 +15,57 @@ const registerUser = asyncHandler(async (req, res) => {
     //chek fro user creation
     //return res
     
+
+    const {fullName, email, username, password} = req.body
+    console.log("email:", email);
+
+    // if (fullName === ""){
+    //     throw new ApiError(400, "fullName is required")
+    // }
+    if (
+        [fullName, email, username, password].some((field) =>
+        field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [ {username} , {email} ]
+    })
+    if (existedUser){
+        throw new ApiError(409, "User already exists")
+    }
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar is required")
+    }
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+    const checkedUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if (!checkedUser) {
+        throw new ApiError(500, "Something when wrong with user registration")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, checkedUser, "User registered")
+    )
 })
 
 export {registerUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,19 @@
-import {router} from "express";
+import {Router} from "express";
+import { registerUser } from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middle.js";
 
-const router = Router()
+const router = Router();
+router.route("register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },{
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
 
-export default router
+export default router;
